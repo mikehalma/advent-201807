9cc1b60120bb14dd088c967c5722f7b276e3f3f7
Mike Halma <mike_halma@yahoo.co.uk>
2019-01-03 18:58:42 +0000

    Get the part1 example working

--- src/main/kotlin/mhalma/advent201807/StepParser.kt
+++ src/main/kotlin/mhalma/advent201807/StepParser.kt
@@ -1,10 +1,61 @@
 package mhalma.advent201807
 
-data class Step(val id: Char, val dependencies: List<Char>)
+import java.lang.IllegalArgumentException
+import java.util.*
 
-fun parseStep(description: String): Step {
+data class Step(val id: Char, val dependencies: MutableSet<Step> = mutableSetOf()) {
+    fun addDependency(dependency: Step) {
+        dependencies.add(dependency)
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (other !is Step) {
+            return false
+        }
+        return id == other.id
+    }
+
+    override fun hashCode(): Int {
+        return this.id.hashCode()
+    }
+
+    override fun toString(): String {
+        return "Step ${this.id}, dependencies ${this.dependencies.map {it.id}.joinToString("")}"
+    }
+}
+
+fun parseStep(description: String): Pair<Char, Char> {
     val regex = """.*([A-Z]).*([A-Z])""".toRegex()
     val result = regex.find(description)
-    val (id, dependency) = result!!.destructured
-    return Step(id.toCharArray()[0], listOf(dependency.toCharArray()[0]))
+    val (dependency, id) = result!!.destructured
+    return Pair(id.toCharArray()[0], dependency.toCharArray()[0])
+}
+
+fun parseSteps(descriptions: List<String>): Set<Step> {
+
+    val steps = mutableSetOf<Step>()
+
+    descriptions.forEach {
+        val (stepId, dependencyId) = parseStep(it)
+        val step = steps.find { it.id == stepId}?:Step(stepId, mutableSetOf())
+        val dependency = steps.find { it.id == dependencyId}?:Step(dependencyId, mutableSetOf())
+        step.addDependency(dependency)
+        if (!steps.contains(step)) steps.add(step)
+        if (!steps.contains(dependency)) steps.add(dependency)
+    }
+
+    return steps
+}
+
+fun calculateStepOrder(descriptions: List<String>): String {
+    val steps = parseSteps(descriptions).toList()
+    Collections.sort(steps) {
+        p0, p1 -> when {
+            p0 == null || p1 == null -> throw IllegalArgumentException("cannot compare null steps")
+            p1.dependencies.contains(p0) -> -1
+            p0.dependencies.contains(p1) -> 1
+            else -> p0.id.compareTo(p1.id)
+        }
+    }
+    return steps.map { it.id }.joinToString("")
 }
--- src/test/kotlin/mhalma/advent201807/StepParserTest.kt
+++ src/test/kotlin/mhalma/advent201807/StepParserTest.kt
@@ -1,14 +1,67 @@
 package mhalma.advent201807
 
 import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.Matchers.containsInAnyOrder
 import org.junit.Assert.assertThat
 import org.junit.Test
+import org.springframework.util.ResourceUtils
 
 class StepParserTest {
 
     @Test
     fun `parse a single step`() {
         val stepDescription = "Step C must be finished before step A can begin."
-        assertThat(parseStep(stepDescription), `is`(Step('C', listOf('A'))))
+        assertThat(parseStep(stepDescription), `is`(Pair('A', 'C')))
+    }
+
+    @Test
+    fun `parse two steps for the same id`() {
+        val stepDescriptions = listOf(
+                "Step A must be finished before step C can begin.",
+                "Step F must be finished before step C can begin."
+        )
+        val stepA = Step('A')
+        val stepF = Step('F')
+        val stepC = Step('C', mutableSetOf(stepA, stepF))
+        assertThat(parseSteps(stepDescriptions), `is`(setOf(stepA, stepC, stepF)))
+    }
+
+    @Test
+    fun `load steps from file`() {
+        val expected = listOf(
+                "Step C must be finished before step A can begin.",
+                "Step C must be finished before step F can begin.",
+                "Step A must be finished before step B can begin.",
+                "Step A must be finished before step D can begin.",
+                "Step B must be finished before step E can begin.",
+                "Step D must be finished before step E can begin.",
+                "Step F must be finished before step E can begin."
+        )
+        assertThat(getStepsFromFile("example.txt"), `is`(expected))
+    }
+
+    private fun getStepsFromFile(fileName: String) = ResourceUtils.getFile("classpath:$fileName").readLines()
+
+    @Test
+    fun `parse example from file`() {
+        val stepC = Step('C')
+        val stepA = Step('A', mutableSetOf(stepC))
+        val stepB = Step('B', mutableSetOf(stepA))
+        val stepD = Step('D', mutableSetOf(stepA))
+        val stepF = Step('F', mutableSetOf(stepC))
+        val stepE = Step('E', mutableSetOf(stepB, stepD, stepF))
+        val expected = setOf(stepE, stepB, stepD, stepF, stepA, stepC)
+
+        val actual = parseSteps(getStepsFromFile("example.txt"))
+        assertThat(actual, containsInAnyOrder(*expected.toTypedArray()))
+        expected.forEach{step ->
+            println("checking step ${step.id}")
+            assertThat(actual.find {it == step}?.dependencies, containsInAnyOrder(*step.dependencies.toTypedArray()))
+        }
+    }
+
+    @Test
+    fun `calculate order of steps`() {
+        assertThat(calculateStepOrder(getStepsFromFile("example.txt")), `is`("CABDFE"))
     }
 }
